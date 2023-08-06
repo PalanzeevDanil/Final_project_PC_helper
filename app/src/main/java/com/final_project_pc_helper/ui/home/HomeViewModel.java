f5c7a7a8ac358b7d49971f9e2438d6f2eb08b349
Danil Palanzeev <palanzeev.dv@gmail.com>
2023-08-06 09:19:32 +0500

    Initial commit

---
+++ app/src/main/java/com/final_project_pc_helper/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.final_project_pc_helper.ui.home;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
